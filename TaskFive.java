5ad62d82a49654e36554a8d7253b78c414ebe401
Andrew <123314186+AndrewKorotkov@users.noreply.github.com>
2023-01-29 21:31:07 +0300

    Add files via upload

---
+++ TaskFive.java
@@ -0,0 +1,23 @@
+public class TaskFive {
+    public static void main(String[] args) {
+        int rows, cols;
+        int a[][] = {{5, 2, 6}, {9, 6, 4}, {4, 15, 3}};
+        rows = a.length;
+        cols = a[0].length;
+
+        if (rows != cols) {
+            System.out.println("Matrix should be a square matrix");
+        } else {
+            System.out.println("Lower triangular matrix: ");
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < cols; j++) {
+                    if (j > i)
+                        System.out.print("0 ");
+                    else
+                        System.out.print(a[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
