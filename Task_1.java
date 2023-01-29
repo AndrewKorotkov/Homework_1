d022d1e0b1212fc3581e193c14cfa8a752e8d5f7
Andrew <123314186+AndrewKorotkov@users.noreply.github.com>
2023-01-29 14:01:48 +0300

    Add files via upload

---
+++ Task_1.java
@@ -0,0 +1,15 @@
+public class Task_1 {
+
+    public static void main(String[] args) {
+
+        int[] array = {3, 5, 7, 12, 8, 9, -5};
+        int n = 8;
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != n)
+                sum = sum + array[i];
+
+        }
+        System.out.println(sum);
+    }
+}
