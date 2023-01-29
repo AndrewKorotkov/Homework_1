e99ab8cd499fae1a0e02bbaa8844ca3948a47a1e
Andrew <123314186+AndrewKorotkov@users.noreply.github.com>
2023-01-29 16:14:36 +0300

    Homework

---
+++ TaskSixMatrixSubtraction.java
@@ -0,0 +1,16 @@
+public class TaskSixMatrixSubtraction {
+    public static void main(String args[]){
+        int a[][]={{1,3,5},{7,9,11},{13,15,17}};
+        int b[][]={{2,4,6},{8,10,12},{14,16,18}};
+
+        int c[][]=new int[3][3];
+
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                c[i][j]=a[i][j]-b[i][j];
+                System.out.print(c[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ TaskThree.java
@@ -0,0 +1,22 @@
+public class TaskThree {
+    public static void main(String[] args) {
+
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        for (int i=0; i<array.length; i++){
+            System.out.print(array[i]);
+        }
+        System.out.println();
+
+        int n = array.length;
+        int reverse;
+
+        for (int i = 0; i < n/2; i++) {
+            reverse = array[n-i-1];
+            array[n-i-1] = array[i];
+            array[i] = reverse;
+        }
+        for (int i=0; i<array.length; i++){
+            System.out.print(array[i]);
+        }
+    }
+}
